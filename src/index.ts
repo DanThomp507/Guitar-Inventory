c48ef15ee21e47ad2f5de70bfde08b444b928141
Daniel Thompson <DanThomp507@gmail.com>
2019-06-11 09:54:33 -0400

    updated scripts in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 const PORT = process.env.PORT || 8080;
+import path from "path";
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+// Configure Express to use EJS
+app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "ejs" );
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    // render the index template
+    res.render( "index" );
+} );
 
 app.listen( PORT, () => {
     // tslint:disable-next-line:no-console
