970ce906f26d677d0c2317cf307f5fbf95321a16
Daniel Thompson <DanThomp507@gmail.com>
2019-06-11 14:57:16 -0400

    added guitar list and updated okta auth

--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,19 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
+const sessionAuth = __importStar(require("./middleware/sessionAuth"));
+const routes = __importStar(require("./routes"));
 // initialize configuration
 dotenv_1.default.config();
 // port is now available to the Node.js runtime
@@ -15,14 +24,11 @@ const app = express_1.default();
 // Configure Express to use EJS
 app.set("views", path_1.default.join(__dirname, "views"));
 app.set("view engine", "ejs");
-// define a route handler for the default home page
-app.get("/", (req, res) => {
-    // render the index template
-    res.render("index");
-});
-// using app.use to serve up static CSS files in public/assets/ folder when /public link is called in ejs files
-// app.use("/route", express.static("foldername"));
-app.use("/public", express_1.default.static("public"));
+// Configure session auth
+sessionAuth.register(app);
+// Configure routes
+routes.register(app);
+// start the express server
 app.listen(PORT, () => {
     // tslint:disable-next-line:no-console
     console.log(`server started at http://localhost:${PORT}`);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
 import path from "path";
+import * as sessionAuth from "./middleware/sessionAuth";
+import * as routes from "./routes";
 
 // initialize configuration
 dotenv.config();
@@ -8,22 +10,20 @@ dotenv.config();
 // port is now available to the Node.js runtime
 // as if it were an environment variable
 const PORT = process.env.SERVER_PORT;
+
 const app = express();
 
 // Configure Express to use EJS
 app.set( "views", path.join( __dirname, "views" ) );
 app.set( "view engine", "ejs" );
 
-// define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    // render the index template
-    res.render( "index" );
-} );
+// Configure session auth
+sessionAuth.register( app );
 
-// using app.use to serve up static CSS files in public/assets/ folder when /public link is called in ejs files
-// app.use("/route", express.static("foldername"));
-app.use("/public", express.static("public"));
+// Configure routes
+routes.register( app );
 
+// start the express server
 app.listen( PORT, () => {
     // tslint:disable-next-line:no-console
     console.log( `server started at http://localhost:${ PORT }` );
