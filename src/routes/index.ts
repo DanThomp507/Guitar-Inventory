4b0bc464afe14e5ed65effc572ca0c421e63ab92
Daniel Thompson <DanThomp507@gmail.com>
2019-06-13 16:56:11 -0400

    updated nav

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,7 +6,7 @@ export const register = ( app: express.Application ) => {
     // define a route handler for the default home page
     app.get( "/", ( req: any, res ) => {
       const user = req.userContext ? req.userContext.userinfo : null;
-        res.render( "index", { isAuthenticated: req.isAuthenticated(), user } );
+      res.render( "index", { isAuthenticated: req.isAuthenticated(), user } );
     } );
 
     // define a secure route handler for the login page that redirects to /guitars
@@ -23,6 +23,6 @@ export const register = ( app: express.Application ) => {
     // define a secure route handler for the guitars page
     app.get( "/guitars", oidc.ensureAuthenticated(), ( req: any, res ) => {
       const user = req.userContext ? req.userContext.userinfo : null;
-        res.render( "guitars", { isAuthenticated: req.isAuthenticated(), user } );
+      res.render( "guitars", { isAuthenticated: req.isAuthenticated(), user } );
     } );
 };
